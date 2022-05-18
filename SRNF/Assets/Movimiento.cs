66df64f01675d91c013f2cfe18637e5236aa1b5e
Sebastian <eabastian@gmail.com>
2022-05-18 07:29:24 -0500

    Added game objects

---
+++ SRNF/Assets/Movimiento.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = ();
+    }
+}
