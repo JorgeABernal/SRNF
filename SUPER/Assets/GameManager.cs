c19f80a6450972e9fe225002b0cc7065dc49041e
Sebastian <eabastian@gmail.com>
2022-05-23 10:02:52 -0500

    Added graph logic

--- SUPER/Assets/GameManager.cs
+++ SUPER/Assets/GameManager.cs
@@ -9,11 +9,21 @@ public class GameManager : MonoBehaviour{
 
     Touch touch,touch2;
     Vector2 touchPosition;
+
+    int pos1, pos2;
+
+    int[,] grafo = new int[5,5]{{0,1,1,1,0},
+                                {1,0,1,0,1},
+                                {1,1,0,1,1},
+                                {1,0,1,0,1},
+                                {0,1,1,1,0}};
+
     int toque = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pos1 = 1;
+        pos2 = 5;
     }
 
     // Update is called once per frame
@@ -29,13 +39,92 @@ public class GameManager : MonoBehaviour{
         if(toque % 2 == 1){
             touch = Input.GetTouch(0);
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-            player.transform.position = new Vector2(touchPos.x,touchPos.y);
+            if(touchPos.x > -2 && touchPos.x < 2 && touchPos.y < 5 && touchPos.y > 2){ // bloque2
+                avanzar(pos1,2,player,1);
+            }else if(touchPos.x < -3 && touchPos.x > -5 && touchPos.y > -2 && touchPos.y < 2){ // bloque1
+                avanzar(pos1,1,player,1);
+            }else if(touchPos.x < 2 && touchPos.x > -2 && touchPos.y < 2 && touchPos.y > -2){ //bloque3
+                avanzar(pos1,3,player,1);
+            }else if(touchPos.x > -2 && touchPos.x < 2 && touchPos.y > -5 && touchPos.y < -3){ //bloque4
+                avanzar(pos1,4,player,1);
+            }else if(touchPos.x > 3 && touchPos.x < 5 && touchPos.y > -2 && touchPos.y < 2){ // bloque5
+                avanzar(pos1,5,player,1);
+            }else{
+                toque--;
+            }
+            
         }else if(toque % 2 == 0){
             touch2 = Input.GetTouch(0);
             Vector2 touchPos2 = Camera.main.ScreenToWorldPoint(touch2.position);
-            player2.transform.position = new Vector2(touchPos2.x,touchPos2.y);
+            if(touchPos2.x > -2 && touchPos2.x < 2 && touchPos2.y < 5 && touchPos2.y > 2){ // bloque2
+                avanzar(pos2,2,player2,2);
+            }else if(touchPos2.x < -3 && touchPos2.x > -5 && touchPos2.y > -2 && touchPos2.y < 2){ // bloque1
+                avanzar(pos2,1,player2,2);
+            }else if(touchPos2.x < 2 && touchPos2.x > -2 && touchPos2.y < 2 && touchPos2.y > -2){ //bloque3
+                avanzar(pos2,3,player2,2);
+            }else if(touchPos2.x > -2 && touchPos2.x < 2 && touchPos2.y > -5 && touchPos2.y < -2){ //bloque4
+                avanzar(pos2,4,player2,2);
+            }else if(touchPos2.x > 3 && touchPos2.x < 5 && touchPos2.y > -2 && touchPos2.y < 2){ // bloque5
+                avanzar(pos2,5,player2,2);
+            }else{
+                toque--;
+            }
         }
         
-
+        void avanzar(int act, int sig, GameObject player,int jug){
+            Debug.Log(act + " " + sig + " " + player.name + " " + jug);
+            if(grafo[act-1,sig-1] == 1){
+                if(pos1-1 != sig-1 && pos2-1 != sig-1){
+                    switch (sig){
+                        case 1:
+                            player.transform.position = new Vector2(-4,1);
+                            if(jug == 1){
+                                pos1 = 1;
+                            }else{
+                                pos2 = 1;
+                            }
+                            break;
+                        case 2:
+                            player.transform.position = new Vector2(0,4);
+                            if(jug == 1){
+                                pos1 = 2;
+                            }else{
+                                pos2 = 2;
+                            }
+                            break;
+                        case 3:
+                            player.transform.position = new Vector2(0,1);
+                            if(jug == 1){
+                                pos1 = 3;
+                            }else{
+                                pos2 = 3;
+                            }
+                            break;
+                        case 4:
+                            player.transform.position = new Vector2(0,-2);
+                            if(jug == 1){
+                                pos1 = 4;
+                            }else{
+                                pos2 = 4;
+                            }
+                            break;
+                        case 5:
+                            player.transform.position = new Vector2(4,1);
+                            if(jug == 1){
+                                pos1 = 5;
+                            }else{
+                                pos2 = 5;
+                            }
+                            break;
+                    }
+                }else{
+                    Debug.Log("pos1/pos2 == sig");
+                    toque--;
+                }
+            }else{
+                Debug.Log("grafo[act,sig] es 0");
+                toque--;
+            }
+        }
     }
 }
